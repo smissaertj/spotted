6a5d5957d90c084a30421abfa180e8ff5e102683
Joeri Smissaert <smissaertj@gmail.com>
2022-09-22 20:44:49 +0400

    Guard /manage routes

--- spotted-client/src/router/index.js
+++ spotted-client/src/router/index.js
@@ -3,6 +3,7 @@ import Home from "@/views/Home.vue";
 import Explore from "@/views/Explore.vue";
 import Manage from "@/views/Manage.vue";
 import NotFound from "@/views/NotFound.vue";
+import useUserStore from "@/stores/user.js";
 
 const routes = [
   {
@@ -15,23 +16,35 @@ const routes = [
   {
     name: "manage",
     path: "/manage/profile",
-    alias: "/manage",
     component: Manage,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     name: "newSubmission",
     path: "/manage/newsubmission",
+    alias: "/manage",
     component: Manage,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     name: "mySubmissions",
     path: "/manage/mysubmissions",
     component: Manage,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     name: "myComments",
     path: "/manage/mycomments",
     component: Manage,
+    meta: {
+      requiresAuth: true,
+    },
   },
   { name: "NotFound", path: "/:pathMatch(.*)*", component: NotFound },
 ];
@@ -40,6 +53,19 @@ const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
   linkExactActiveClass: "text-yellow-500",
+});
+
+router.beforeEach((to, from, next) => {
+  if (!to.meta.requiresAuth) {
+    next();
+    return;
+  }
+  const store = useUserStore();
+  if (store.userLoggedIn) {
+    next();
+  } else {
+    next({ name: "home" });
+  }
 });
 
 export default router;
