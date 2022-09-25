d07c0594cbb92f08e06ac58cd1d95b56f3ab9a53
Joeri Smissaert <smissaertj@gmail.com>
2022-09-25 09:03:31 +0400

    Render user data in table. Disable action buttons if user is not selected.

--- spotted-client/src/components/manageUsers.vue
+++ spotted-client/src/components/manageUsers.vue
@@ -1,166 +1,51 @@
 <template>
   <div class="overflow-x-auto w-full">
-    <table class="table w-full">
+    <table class="table w-100 m-auto">
       <!-- head -->
       <thead>
         <tr>
           <th>
-            <label>
-              <input type="checkbox" class="checkbox" />
-            </label>
+            <label> Select </label>
           </th>
-          <th>Name</th>
-          <th>Job</th>
-          <th>Favorite Color</th>
-          <th></th>
+          <th>User</th>
+          <th>Status</th>
+          <th>Actions</th>
         </tr>
       </thead>
       <tbody>
         <!-- row 1 -->
-        <tr>
+        <tr v-for="user in users" :key="user.uid">
           <th>
             <label>
-              <input type="checkbox" class="checkbox" />
+              <input type="checkbox" class="checkbox" v-model="user.selected" />
             </label>
           </th>
           <td>
             <div class="flex items-center space-x-3">
-              <div class="avatar">
-                <div class="mask mask-squircle w-12 h-12">
-                  <img
-                    src="https://via.placeholder.com/150"
-                    alt="Profile Picture"
-                  />
-                </div>
-              </div>
               <div>
-                <div class="font-bold">Hart Hagerty</div>
-                <div class="text-sm opacity-50">United States</div>
+                <div class="font-bold">{{ user.displayName }}</div>
+                <div class="text-sm opacity-50">{{ user.email }}</div>
               </div>
             </div>
           </td>
+          <td>{{ user.isDisabled ? "Disabled" : "Active" }}</td>
           <td>
-            Zemlak, Daniel and Leannon
-            <br />
-            <span class="badge badge-ghost badge-sm"
-              >Desktop Support Technician</span
-            >
+            <button class="btn btn-ghost btn-xs" :disabled="!user.selected">
+              Delete
+            </button>
+            <button class="btn btn-ghost btn-xs" :disabled="!user.selected">
+              Deactivate
+            </button>
           </td>
-          <td>Purple</td>
-          <th>
-            <button class="btn btn-ghost btn-xs">details</button>
-          </th>
-        </tr>
-        <!-- row 2 -->
-        <tr>
-          <th>
-            <label>
-              <input type="checkbox" class="checkbox" />
-            </label>
-          </th>
-          <td>
-            <div class="flex items-center space-x-3">
-              <div class="avatar">
-                <div class="mask mask-squircle w-12 h-12">
-                  <img
-                    src="https://via.placeholder.com/150"
-                    alt="Profile Picture"
-                  />
-                </div>
-              </div>
-              <div>
-                <div class="font-bold">Brice Swyre</div>
-                <div class="text-sm opacity-50">China</div>
-              </div>
-            </div>
-          </td>
-          <td>
-            Carroll Group
-            <br />
-            <span class="badge badge-ghost badge-sm">Tax Accountant</span>
-          </td>
-          <td>Red</td>
-          <th>
-            <button class="btn btn-ghost btn-xs">details</button>
-          </th>
-        </tr>
-        <!-- row 3 -->
-        <tr>
-          <th>
-            <label>
-              <input type="checkbox" class="checkbox" />
-            </label>
-          </th>
-          <td>
-            <div class="flex items-center space-x-3">
-              <div class="avatar">
-                <div class="mask mask-squircle w-12 h-12">
-                  <img
-                    src="https://via.placeholder.com/150"
-                    alt="Profile Picture"
-                  />
-                </div>
-              </div>
-              <div>
-                <div class="font-bold">Marjy Ferencz</div>
-                <div class="text-sm opacity-50">Russia</div>
-              </div>
-            </div>
-          </td>
-          <td>
-            Rowe-Schoen
-            <br />
-            <span class="badge badge-ghost badge-sm">Office Assistant I</span>
-          </td>
-          <td>Crimson</td>
-          <th>
-            <button class="btn btn-ghost btn-xs">details</button>
-          </th>
-        </tr>
-        <!-- row 4 -->
-        <tr>
-          <th>
-            <label>
-              <input type="checkbox" class="checkbox" />
-            </label>
-          </th>
-          <td>
-            <div class="flex items-center space-x-3">
-              <div class="avatar">
-                <div class="mask mask-squircle w-12 h-12">
-                  <img
-                    src="https://via.placeholder.com/150"
-                    alt="Profile Picture"
-                  />
-                </div>
-              </div>
-              <div>
-                <div class="font-bold">Yancy Tear</div>
-                <div class="text-sm opacity-50">Brazil</div>
-              </div>
-            </div>
-          </td>
-          <td>
-            Wyman-Ledner
-            <br />
-            <span class="badge badge-ghost badge-sm"
-              >Community Outreach Specialist</span
-            >
-          </td>
-          <td>Indigo</td>
-          <th>
-            <button class="btn btn-ghost btn-xs">details</button>
-          </th>
         </tr>
       </tbody>
       <!-- foot -->
       <tfoot>
         <tr>
-          <th></th>
-          <th>Name</th>
-          <th>Job</th>
-          <th>Favorite Color</th>
-          <th></th>
+          <th>Select</th>
+          <th>User</th>
+          <th>Status</th>
+          <th>Actions</th>
         </tr>
       </tfoot>
     </table>
